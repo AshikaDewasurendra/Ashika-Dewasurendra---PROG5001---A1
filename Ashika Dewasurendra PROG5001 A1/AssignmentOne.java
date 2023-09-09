80b62ba125b856d751761d75b6112a3815fcd401
Ashika Dewasurendra <ashivv1@gmail.com>
2023-09-09 18:45:13 +0800

    update

--- Ashika Dewasurendra PROG5001 A1/AssignmentOne.java
+++ Ashika Dewasurendra PROG5001 A1/AssignmentOne.java
@@ -1,33 +1,21 @@
 
+import java.util.*;
 /**
  * Write a description of class AssignmentOne here.
  *
- * @author (your name)
+ * @author (Ashika Dewasurendra)
  * @version (a version number or a date)
  */
 public class AssignmentOne
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class AssignmentOne
-     */
-    public AssignmentOne()
-    {
-        // initialise instance variables
-        x = 0;
+ public static void main(String []args){
+    Scanner AssignmentOne = new Scanner(System.in);
+    double [] assignmentMarks = new double[30];
+    
+    
+    
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
+    
+    
 }
